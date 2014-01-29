c32448b8808c41158983085143876fd8706fec57
Damian Serrano Thode <mail@dsthode.info>
2014-01-29 22:35:55 +0100

    fixes the date

--- post_list.php
+++ post_list.php
@@ -4,7 +4,7 @@
 			<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('post_list_thumbnail', array('class' => 'post-list-thumbnail-image')); ?></a>
 		</div>
 		<div class="col-sm-8">
-			<p class="post-list-meta"><?php the_date(); ?> | <span class="post-list-comments"><?php comments_number('Sin comentarios', 'Un comentario', '% comentarios'); ?></span></p>
+			<p class="post-list-meta"><?php echo get_the_date(); ?> | <span class="post-list-comments"><?php comments_number('Sin comentarios', 'Un comentario', '% comentarios'); ?></span></p>
 			<h3 class="post-list-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
 			<?php the_excerpt(); ?>
 			<span class="seguir-leyendo"><a class="btn btn-default" href="<?php the_permalink(); ?>">SEGUIR LEYENDO &raquo;</a></span>
