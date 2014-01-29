7bc73753c6d2e122c1091dc84b8aadfeadad1d90
Damian Serrano Thode <mail@dsthode.info>
2014-01-30 00:47:02 +0100

    modified translator loading

--- footer.php
+++ footer.php
@@ -17,13 +17,5 @@
 <?php wp_footer(); ?>
 <script src="<?php bloginfo('template_url'); ?>/script.js" type="text/javascript"></script>
 <?php include('analytics.php'); ?>
-<script type="text/javascript">
-function googleTranslateElementInit() { new google.translate.TranslateElement({pageLanguage: 'es'}, 'google_translate_element'); } 
-(function(){
-	var tra= document.createElement('script'); tra.type="text/javascript"; tra.async=true;
-	tra.src= ('https:' == document.location.protocol ? 'https:' : 'http:') + "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
-	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(tra, s);
-})();
-</script>
 </body>
 </html>
