246b92d6e006800c28fc1131671c33eb47223630
Damian Serrano Thode <dsthode@gmail.com>
2014-04-16 16:15:58 +0200

    fixed jquery

--- header.php
+++ header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+<?php wp_enqueue_script('jquery'); ?>
 <head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# article: http://ogp.me/ns/article#">
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width, initial-scale=1" />
