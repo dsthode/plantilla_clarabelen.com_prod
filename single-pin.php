ef34a60c9de6e1948f1426291963567b18fe016f
Damian Serrano Thode <dsthode@gmail.com>
2014-08-30 11:59:28 +0200

    Added pin custom post type page and fixed microformats

--- single-pin.php
+++ single-pin.php
@@ -13,7 +13,7 @@
 		<?php if(have_posts()) : ?>
 			<?php while(have_posts()) : the_post(); ?>
 				<div class="post hentry">
-					<p class="postmetadata"><span class="updated"><?php the_date(); ?></span> | <?php comments_number('Sin comentarios', 'Un comentario', '% comentarios'); ?> | por <span class="author vcard"><span class="fn"><?php the_author(); ?></span></span></p>
+					<p class="postmetadata"><span class="published"><?php the_date(); ?></span> | <?php comments_number('Sin comentarios', 'Un comentario', '% comentarios'); ?> | por <span class="author vcard"><span class="fn"><?php the_author(); ?></span></span></p>
 					<h3 class="entry-title"><a class="permalink" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
 					<div class="sharing">
 						<div>Compártelo en:</div>
@@ -21,8 +21,12 @@
 						<a target="_blank" href="http://twitter.com/share?text=<?php urlencode(the_title()); ?>&url=<?php urlencode(the_permalink()); ?>" alt="Compartir en Twitter"><div class="icon-twitter sprite-icon"></div></a>
 						<a target="_blank" href="https://plus.google.com/share?url=<?php urlencode(the_permalink()); ?>" alt="Compartir en Google+"><div class="icon-googleplus sprite-icon"></div></a>
 					</div>
-					<div class="entry entry-content">   
+					<div class="entry entry-content">
+						<?php $post_custom = get_post_custom(); ?>
 						<?php the_content(); ?>
+						<p><a href="<?php echo($post_custom["pin_source_url"][0]); ?>">Visitar el artículo original</a></p>
+						<p><a href="<?php echo($post_custom["pin_source_url"][0]); ?>"><img src="<?php echo($post_custom["pin_source_image_url"][0]); ?>"></a></p>
+						<br/>
 					</div>
 					<div class="sharing">
 						<div>Compártelo en:</div>
